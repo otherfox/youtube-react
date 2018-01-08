00ef2cd809d14c9981b0b829bdd4a3dedd103a8f
Nick Tennies <tenniesdesign@gmail.com>
2018-01-08 08:33:30 -0500

    adds gets videos as state

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCQCTUS0ohMlJr8eHozsD2p-OTk5Tyh76s';
-YTSearch({ key: API_KEY, term: 'surf boards' }, function(data) {
-  console.log(data);
-});
 
 // Create a new component which creates some HTML
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Componenet {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({ key: API_KEY, term: 'surf boards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Put component's generate HTML and put on the page (in the DOM)
